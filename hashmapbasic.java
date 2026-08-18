a3be9ecc440e3bdd14c99ce5a8f3f85f40eb8a14
Harryptdr <phariom215@gmail.com>
2026-08-18 14:43:30 +0530

    basic data structure concept

--- hashmapbasic.java
+++ hashmapbasic.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class hashmapbasic {
     public static void main(String[] args) {
-        int [] arr={1,2,3,1,4,1};
+        int [] arr={1,2,3,1,4,1,5,7};
 
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
